7bb0f43e8c831f6cec45c0076e79367281730830
Andrew Zures <andrew.zures@gmail.com>
2021-07-06 13:25:13 -0500

    wip: object detection connection

--- src/Service/Image.hs
+++ src/Service/Image.hs
@@ -1,11 +1,27 @@
 module Service.Image where
 
+import Control.Lens
 import Control.Monad.IO.Class (liftIO)
+import Data.ByteString
+import Data.Text
+import Data.Text.Encoding
 import Model.Image (Image (Image))
+import qualified Network.AWS.Rekognition.DetectLabels as R
+import qualified Network.AWS.Rekognition.Types as T
 
 fetchAndAttachDetectedObjects :: Image -> IO Image
 fetchAndAttachDetectedObjects image = do
   return image
+
+-- toUTF8Bytes = Data.ByteString.unpack . encodeUtf8 . Data.Text.pack
+
+-- fetchLabels :: Image -> T.Image
+-- fetchLabels (Image _ _ _ src _ _ _ _) =
+--   let img = toUTF8Bytes src :: T.Image
+
+-- R.detectLabels rimage
+
+-- fetchImage
 
 -- fetchDetectedObjects :: Image -> IO [String]
 
